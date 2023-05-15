43733dd8f60acd56dbb2bb1c376cee28c6e740af
Сункар Бауыржанулы <sunkar2710@gmail.com>
2023-05-15 12:47:18 +0600

    feat: implement stage1-module6-io-task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,47 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileReader {
+    private HashMap<String, String> map = new HashMap<>();
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        StringBuilder text = new StringBuilder();
+
+        try (FileInputStream fileInputStream = new FileInputStream(file)){
+            int ch;
+            while((ch=fileInputStream.read()) != -1) {
+                text.append((char) ch);
+            }
+        } catch (IOException ex) {
+            // exception handling
+        }
+
+        map = parseDataFromFile(text.toString());
+
+        return new Profile(map.get("Name"),
+                Integer.parseInt(map.get("Age")),
+                map.get("Email"),
+                Long.parseLong(map.get("Phone")));
+    }
+
+    private HashMap<String, String> parseDataFromFile(String text) {
+        String regexKey = "^(.*)(?=:)";
+        String regexValue = "(?<=: )(.*)";
+
+        Pattern patternKey = Pattern.compile(regexKey, Pattern.MULTILINE);
+        Pattern patternValue = Pattern.compile(regexValue);
+
+        Matcher matcherKey = patternKey.matcher(text);
+        Matcher matcherValue = patternValue.matcher(text);
+
+        while (matcherKey.find() && matcherValue.find()) {
+            map.put(text.substring(matcherKey.start(), matcherKey.end()), text.substring(matcherValue.start(), matcherValue.end()));
+        }
+        return map;
     }
 }
